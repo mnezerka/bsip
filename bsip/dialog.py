6f160f6238d312ab34925ee5b23703392c61a2c4
michal.nezerka <michal.nezerka@gmail.com>
2014-10-01 11:20:20 +0000

    Development

---
+++ bsip/dialog.py
@@ -0,0 +1,8 @@
+class Dialog:
+    """SIP Dialog
+    
+    Dialog is a peer-to-peer SIP relationship between two UAs that persists
+    for some time. A dialog is identified by a Call-ID, a local tag and
+    a remote tag. A dialog used to be referred as a 'call leg'."""
+
+    pass
