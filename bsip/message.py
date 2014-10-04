7fe495572fc8f10c548f0989d94a041769888d6e
michal.nezerka <michal.nezerka@gmail.com>
2014-10-04 21:44:58 +0000

    Development

--- bsip/message.py
+++ bsip/message.py
@@ -813,34 +813,7 @@ class ContentTypeHeader(Header, MediaType):
         return result
 
 class ExpiresHeader(Header):
-    """The Expires header field gives the relative time after which the message (or content)
-    expires.
-
-    The precise meaning of this is method dependent. The expiration time in an INVITE
-    does not affect the duration of the actual session that may result from the invitation.
-    Session description protocols may offer the ability to express time limits on the session
-    duration, however. The value of this field is an integral number of seconds (in decimal)
-    between 0 and (2**32)-1, measured from the receipt of the request. Malformed values
-    SHOULD be treated as equivalent to 3600.
-
-    This interface represents the Expires entity-header. The ExpiresHeader is optional in
-    both REGISTER and INVITE Requests.
-
-     * REGISTER - When a client sends a REGISTER request, it MAY suggest an expiration interval
-    that indicates how long the client would like the registration to be valid. There are two ways
-    in which a client can suggest an expiration interval for a binding: through an Expires header
-    field or an "expires" Contact header parameter. The latter allows expiration intervals to be
-    suggested on a per-binding basis when more than one binding is given in a single REGISTER request,
-    whereas the former suggests an expiration interval for all Contact header field values that do not
-    contain the "expires" parameter.
-
-     * INVITE - The UAC MAY add an Expires header field to limit the validity of the invitation. If the
-    time indicated in the Expires header field is reached and no final answer for the INVITE has
-    been received, the UAC core SHOULD generate a CANCEL request for the INVITE. 
-
-    Example:
-    Expires: 5
-    """
+    """The Expires header field gives the relative time after which the message (or content) expires."""
     
     def __init__(self, body = None):
         Header.__init__(self, 'Expires' , body)
--- bsip/uac.py
+++ bsip/uac.py
@@ -10,53 +10,132 @@ import user
 import auth
 import transaction
 from transaction import Transaction
-from bsip.sip import Sip
+from sip import Sip, SipException
 
 class UAC(stack.Module):
     """UAC module"""
+
+    LOGGER_NAME = 'BSip.UAC'
+
+    STATE_NOT_REGISTERED = 'not-registered'
+    STATE_REGISTERING = 'registering'
+    STATE_REGISTERED = 'registered'
+    STATE_DEREGISTERING = 'de-registering'
 
     def __init__(self, user):
         stack.Module.__init__(self)
         self.priority = stack.Module.PRIO_DIALOG_USAGE
         self.user = user
         self.digestAuthenticator = auth.DigestAuthenticator(user)
+        self.state = UACNotRegistered(self)
+        self.logger = logging.getLogger(self.LOGGER_NAME)
 
     def getId(self):
-        return 'reg'
+        """Returns module identification"""
+        return 'uac'
+
+    def getState(self):
+        return self.state.getId()
+
+    def setState(self, newState):
+        self.logger.debug('Changing state: %s -> %s' % (self.state.getId(), newState.getId()))
+        self.state = newState
 
     def register(self):
-        regRequest = message.MessageFactory.createRequestRegister(self.user.getAddress())
-        trans = transaction.TranClientNonInvite(self.stack, self, regRequest, self.user.getProxy())
-        trans.sendRequest()
-       
-    # Called on rx response
-    def onRxResponse(self, rxData):
-        print 'Received SIP response: %d %s' % (rxData.msg.getStatusCode(), rxData.msg.getReasonPhrase())
+        self.state.register()
 
-        # if received response is of type "some authorization required", we need to generate new
-        # request with computed authentication values 
-        if rxData.msg.getStatusCode() in [Sip.RESPONSE_UNAUTHORIZED, Sip.RESPONSE_PROXY_AUTHENTICATION_REQUIRED]:
-            request = handleChallenge(rxData.msg, self.origRequest)
-            txData = stack.SipTxData()
-            txData.transport = self.stack.acquireTransport(sip.Sip.TRANSPORT_UDP)
-            txData.dest = user.getProxy() 
-            self.stack.sendStateless(txData)
-        else:
-            print "finished"
-
-        return False
+    def deRegister(self):
+        self.state.deRegister()
 
     def onTranState(self, tran):
-        print 'Transaction state have changed to ', tran.state.getId()
+        self.logger.debug('Transaction state have changed to ' + tran.state.getId())
+        self.state.onTranState(tran)
 
+class UACState():
+    """Base class for all UAC states"""
+
+    def __init__(self, uac):
+        self.uac = uac
+
+    def getId(self):
+        return 'base'
+
+    def register(self):
+        raise SipException('Cannot register in this state')
+
+    def deRegister(self):
+        raise SipException('Cannot de-register in this state')
+
+    def onTranState(self, tran):
+        pass
+         
+class UACNotRegistered(UACState):
+    """User is not registered"""
+
+    def getId(self):
+        return UAC.STATE_NOT_REGISTERED
+
+    def register(self):
+        self.uac.logger.info('Registering user')
+        regRequest = message.MessageFactory.createRequestRegister(self.uac.user.getAddress())
+        trans = transaction.TranClientNonInvite(self.uac.stack, self.uac, regRequest, self.uac.user.getProxy())
+        trans.sendRequest()
+        self.uac.setState(UACRegistering(self.uac))
+         
+class UACRegistering(UACState):
+    """User is being registered"""
+
+    def getId(self):
+        return UAC.STATE_REGISTERING
+
+    def onTranState(self, tran):
         if tran.state.getId() == Transaction.STATE_COMPLETED:
-            print tran.getLastStatusCode()
+            self.uac.logger.debug("transaction completed with sip status code: %d" % tran.getLastStatusCode())
 
             # check if authentication is required
-            if tran.getLastStatusCode() == 401:
-                regRequest2 = self.digestAuthenticator.handleChallenge(tran.lastResponse, tran.originalRequest)
-                print regRequest2
-                #trans = transaction.TranClientNonInvite(self.stack, self, regRequest, self.user.getProxy())
-                #trans.sendRequest()
-     
+            if tran.getLastStatusCode() in [401, 403]:
+                regRequest2 = self.uac.digestAuthenticator.handleChallenge(tran.lastResponse, tran.originalRequest)
+                self.uac.digestAuthenticator.onMessageSend(regRequest2)
+                trans = transaction.TranClientNonInvite(self.uac.stack, self.uac, regRequest2, self.uac.user.getProxy())
+                trans.sendRequest()
+            elif tran.getLastStatusCode() == 200:
+                self.uac.setState(UACRegistered(self.uac))
+            else:
+                SipException('Unexpected reponse code: %d' % tran.getLastStatusCode())
+
+class UACRegistered(UACState):
+    """User is registered"""
+
+    def getId(self):
+        return UAC.STATE_REGISTERED
+
+    def deRegister(self):
+        deRegRequest = message.MessageFactory.createRequestRegister(self.uac.user.getAddress())
+        expiresHeader = deRegRequest.getHeaderByType(message.ExpiresHeader)
+        expiresHeader.setExpires(0)
+        trans = transaction.TranClientNonInvite(self.uac.stack, self.uac, deRegRequest, self.uac.user.getProxy())
+        trans.sendRequest()
+        self.uac.setState(UACDeRegistering(self.uac))
+
+class UACDeRegistering(UACState):
+    """User is being deregistered"""
+
+    def getId(self):
+        return UAC.STATE_DEREGISTERING
+
+    def onTranState(self, tran):
+        if tran.state.getId() == Transaction.STATE_COMPLETED:
+            self.uac.logger.debug("transaction completed with sip status code: %d" % tran.getLastStatusCode())
+
+            # check if authentication is required
+            if tran.getLastStatusCode() in [401, 403]:
+                regRequest2 = self.uac.digestAuthenticator.handleChallenge(tran.lastResponse, tran.originalRequest)
+                self.uac.digestAuthenticator.onMessageSend(regRequest2)
+                trans = transaction.TranClientNonInvite(self.uac.stack, self.uac, regRequest2, self.uac.user.getProxy())
+                trans.sendRequest()
+            elif tran.getLastStatusCode() == 200:
+                self.uac.setState(UACNotRegistered(self.uac))
+            else:
+                SipException('Unexpected reponse code: %d' % tran.getLastStatusCode())
+
 
